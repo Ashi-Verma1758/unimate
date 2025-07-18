5b934ad12cc47d3e927ec14847305412c6cffeb4
Ashi-Verma1758 <ashiverma1758@gmail.com>
2025-07-18 18:03:28 +0530

    solved the conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-
-import React from 'react'
-import './App.css';
-
-
-import HeroSection from './components/HeroSection/HeroSection'
-
+import './App.css'
+import HeroSection from './components/HeroSection/HeroSection.jsx'
+// import Navbar from './components/Navbar'
 function App() {
   return (
     <>
     <HeroSection/>
+    {/* <Navbar/> */}
     </>
   )
 }
