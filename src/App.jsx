67c4ac1b5d099ebe1e03456428ba2e58f5ab84a4
Ashi-Verma1758 <ashiverma1758@gmail.com>
2025-07-24 11:55:54 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import React from 'react'
 import './App.css';
 
+// import Login from './Log-In';
 // import Navbar from './components/Navbar.jsx';
 import HeroSection from './components/HeroSection/HeroSection'
-// import { Sidebar } from 'lucide-react';
-// import TeamCard from './components/teamInvite/TeamCard';
-// import Sidebar from './components/ChatBox/Sidebar';
-import ChatWindow from './components/ChatBox/ChatWindow'
-import TeamMembers from './components/ChatBox/TeamMembers';
-import Sidebar from './components/ChatBox/SideBar';
+import CreateAccount from './components/CreateAcc';
+
+
+import HomePage from './components/HomePage/HomePage';
+import Login from './Log-In';
+
+
 function App() {
   return (
-    <>
-    {/* <Navbar/> */}
-    {/* <HeroSection/> */}
-    {/* <TeamCard/> */}
-    <div className="parent-container">
-     <Sidebar/>
-     <ChatWindow/>
-      <TeamMembers/>
-    </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroSection/>} />
+        <Route path="/signup" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/Homepage" element={<HomePage />} />
+
+      </Routes>
+    </Router>
+ 
   )
 }
 
